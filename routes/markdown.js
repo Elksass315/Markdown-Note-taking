2b1e9a3e0b8f56f49bb3eecbcd3fb3b1349d4961
Elksass315 <qsasomar@gmail.com>
2024-09-28 01:52:45 +0300

    New: get files list and file by id

--- routes/markdown.js
+++ routes/markdown.js
@@ -3,12 +3,18 @@ import multer from "multer";
 import multerStorage from "../helpers/multerStorage.js";
 import auth from "../middleware/auth.js";
 import userFiles from "../model/userFile.js";
+import winston from "winston";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const router = express.Router();
 const upload = multer({ storage: multerStorage, limits: { fileSize: 1000000 } });
 
-router.post("/upload", [auth, upload.single("file")], async(req, res) => {
-
+router.post("/upload", [auth, upload.single("file")], async (req, res) => {
 
     if (req.file) {
         if (req.file.originalname.match(/\.(md|markdown)$/i) === null) {
@@ -17,17 +23,13 @@ router.post("/upload", [auth, upload.single("file")], async(req, res) => {
         }
 
         let userFile = await userFiles.findOne({ User: req.user._id });
-
-        // If the user has files already, push the new file
         if (userFile) {
             if (!userFile.files) {
-                // Ensure the 'files' array exists
                 userFile.files = [];
             }
             userFile.files.push(req.file.path);
             await userFile.save();
         } else {
-            // If the user doesn't have files, create a new entry
             const newUserFile = new userFiles({
                 User: req.user._id,
                 files: [req.file.path]
@@ -40,6 +42,38 @@ router.post("/upload", [auth, upload.single("file")], async(req, res) => {
         res.json({ file: req.file });
     } else {
         res.status(400).json({ error: "No file uploaded" });
+    }
+});
+
+
+router.get("/files-list", auth, async (req, res) => {
+    let userFile = await userFiles.findOne({ User: req.user._id });
+    if (userFile) {
+        res.json({ files: userFile.files });
+    } else {
+        res.status(404).json({ error: "No files found" });
+    }
+});
+
+router.get("/file/:fileid", auth, async (req, res) => {
+    let userFile = await userFiles.findOne({ User: req.user._id });
+    if (userFile) {
+
+        const requestedFile = `uploads\\${req.params.fileid}`;
+
+        if (userFile.files.includes(requestedFile)) {
+            const filePath = path.join(__dirname, '..', 'uploads', req.params.fileid);
+            res.sendFile(filePath, (err) => {
+                if (err) {
+                    winston.error(err);
+                    res.status(500).json({ error: "Could not send the file" });
+                }
+            });
+        } else {
+            res.status(404).json({ error: "File not found" });
+        }
+    } else {
+        res.status(404).json({ error: "No files found" });
     }
 });
 
